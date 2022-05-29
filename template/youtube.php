98f9486a76ad793bc17b9dfd9f9b28ced58d3b6d
sashilshrestha <sashilstha5@gmail.com>
2022-05-30 00:07:02 +0545

    yotube section

--- template/youtube.php
+++ template/youtube.php
@@ -15,26 +15,14 @@ get_header();
     </div>
     <div class="gallery--content">
         <ul class="links">
-            <li><a data-filter="*" class="active">All</a></li>
-            <?php
-            $taxonomy = get_terms('photos_category', array('hide_empty' => 0));
-            foreach ($taxonomy as $taxo) {
-
-            ?>
-                <li><a data-filter=".<?php echo $taxo->name ?>"><?php echo $taxo->name ?></a></li>
-            <?php
-            }
-            ?>
-
-
-
+            <li><a data-filter="*" class="active">Refresh</a></li>
         </ul>
         <div class="gallery--container">
             <div class="container wrapper">
                 <!-- Post Calling Loop Started -->
                 <?php
                 $args = array(
-                    'post_type' => 'photos',
+                    'post_type' => 'youtube',
                     'posts_per_page' => -1,
                     'order' => 'ASC',
                     'orderby' => 'menu_order',
@@ -46,17 +34,14 @@ get_header();
                     // For Image Call
                     $thumb_id = get_post_thumbnail_id();
                     $thumb_url = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
-                    $categories = get_the_terms($post->ID, 'photos_category');
-
                 ?>
                     <!-- Loop Started -->
-                    <div class="<?php print_r($categories[0]->name); ?> item"><a href="<?php echo $thumb_url[0]; ?>" class="magnify"><img src="<?php echo $thumb_url[0]; ?>" alt=""></a></div>
+                    <div class="item"><a href="<?php the_title() ?>" class="videos-frame"><img src="<?php echo $thumb_url[0]; ?>" alt=""></a></div>
                     <!-- Loop Ended -->
                 <?php
                 endwhile;
                 wp_reset_postdata();
                 ?>
-                <div class="item"><a href="https://www.youtube.com/watch?v=06hKnu9RUKk" class="videos-frame"><img src="https://i.ytimg.com/vi/06hKnu9RUKk/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCKcuasF0VSv4Z1uZJodGtv5SyJzA" alt=""></a></div>
             </div>
         </div>
 
